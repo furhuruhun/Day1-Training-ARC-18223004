6e8cf827d5888a7a4d3594d4f7664e7749ea126e
furhuruhun <18223004@std.stei.itb.ac.id>
2025-02-01 16:44:18 +0700

    add:produk navbar

--- KisahManis/JS/script.js
+++ KisahManis/JS/script.js
@@ -1,14 +1,31 @@
 const navbarNav = document.querySelector('.navbar-nav');
 const hamburgerMenu = document.querySelector('#hamburger-menu');
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+const searchbut = document.querySelector('#search-button');
 
+// Toggle class
 //biar kalo hamburger menu di klik muncul sidebar
 hamburgerMenu.addEventListener('click', () => {
     navbarNav.classList.toggle('active');
 });
 
+//buat searchform
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+}
+
 //biar kalo pas buka sidebar trus lu pencet di selain sidebar nnti keclose side bar nya
 document.addEventListener('click', function(e) {
+    //biar kalo pas buka sidebar trus lu pencet di selain sidebar nnti keclose side bar nya
     if(!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
+    }
+
+    //biar searchbutton kalo di klik di luar itu ketutup
+    if(!searchbut.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
     }
 })
